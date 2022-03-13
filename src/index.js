473c5d8e60cbc55a1b46fd5511ec16270de2f8e6
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-03-12 22:04:00 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -160,7 +160,7 @@ socket.on("wireguardConfig", writeWireguardConfig)
  */
 async function writeWireguardConfig(config){
   const {users, WireguardIpConfig} = config;
-  console.log(JSON.stringify(config));
+  return console.log(JSON.stringify(config));
   const NetInterfaces = networkInterfaces();
   const PostUp = [
     `iptables -A FORWARD -i ${NetInterfaces[0].interface} -o wg0 -j ACCEPT`,
