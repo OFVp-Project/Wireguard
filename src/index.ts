3d3bf07006f6f5ec227368bd9d6c25ee6fabf967
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-04-06 20:33:26 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as socketIo from "socket.io-client";
 import * as Wireguard from "./Wireguard";
 const { DAEMON_PASSWORD, DAEMON_USER, DAEMON_HOST } = process.env;
 
-const ioClient = socketIo.io(DAEMON_HOST, {auth: {username: DAEMON_USER, password: DAEMON_PASSWORD}});
+const ioClient = socketIo.io(DAEMON_HOST, {transports: [ "websocket", "polling" ],auth: {username: DAEMON_USER, password: DAEMON_PASSWORD}});
 ioClient.on("conneted", () => console.info("Connected to daemon"));
 ioClient.on("disconnect", () => console.info("Disconnected from daemon"));
 ioClient.on("error", err => {
